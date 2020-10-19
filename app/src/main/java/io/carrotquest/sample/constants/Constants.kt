77a212cc4b80e21083bc1784f59a48664aae43fa
botalov <botalov.n@carrotquest.io>
2020-10-19 16:44:02 +0500

    * Поднята версия

--- app/src/main/java/io/carrotquest/sample/constants/Constants.kt
+++ app/src/main/java/io/carrotquest/sample/constants/Constants.kt
@@ -1,8 +1,11 @@
 package io.carrotquest.sample.constants
 
-const val API_KEY = ""
-const val APP_ID = ""
-const val USER_AUTH_KEY = ""
+/*const val API_KEY = "3060-ed66f34a99b114d5aa7161b3619"
+const val APP_ID = "3060"
+const val USER_AUTH_KEY = "userauthkey-3060-2fd1047cb540b573999a9d101a9e7f54a50cf54d380bea175b1c9adf95b6252"*/
+const val API_KEY = "2782-e9c7af5f18d3e057818a91b354a"
+const val APP_ID = "2782"
+const val USER_AUTH_KEY = "userauthkey-2782-cbf299eda136c72f126a8882969f9da6c9dbfb531250078962ae24810dca518"
 
 
 const val API_KEY_SP = "api_key"
