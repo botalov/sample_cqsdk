cba50a8bc72470038aa8c27f40fc1754964be849
botalov <botalov.n@carrotquest.io>
2021-01-13 16:20:14 +0500

    * CICD

--- app/src/androidTest/java/io/carrotquest/sample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/carrotquest/sample/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.carrotquest.sample", appContext.packageName)
+        assertEquals("io.carrotquest.sampleйцвйцв", appContext.packageName)
     }
 }
